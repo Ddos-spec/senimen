4884550314b732c2aafef3bbcf867919eb97eabe
Ddos-spec <setgraph69@gmail.com>
2026-08-10 05:25:29 +0700

    build: add Astro GitHub Pages config

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://ddos-spec.github.io',
+  base: '/senimen',
+  output: 'static',
+  trailingSlash: 'ignore',
+});
